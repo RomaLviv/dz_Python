7e45d528f0986a48e03b91e1c5993419a8d35db9
RomaLviv <chajkovskyj.roman@gmail.com>
2018-02-04 00:36:27 +0200

    Add projectHomeWork

---
+++ dz.py
@@ -0,0 +1,23 @@
+#1
+
+#a = int(input ("Enter first number: "))
+#b = int(input ("Enter second number: "))
+#c = int(input ("Enter third number: "))
+#
+#if a - b <= c and c <= a + b:
+#    print(True)
+#else:
+#    print(False)	
+
+
+#2
+
+#n = int(input ("Enter number: "))
+#if n == 0:
+#    print(0)
+#else:
+#    a, b = 0, 1
+#for i in range(2, n + 1):	
+#    a, b = b, a + b
+#if i > b or i < b:
+#    print(b)	
